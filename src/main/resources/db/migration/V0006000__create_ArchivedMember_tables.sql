b64d1576096a374946124e7ba328657126665bff
Default <HunHage@gmail.com>
2021-03-17 02:02:31 -0500

    working ArchivedMemberFactory

--- src/main/resources/db/migration/V0006000__create_ArchivedMember_tables.sql
+++ src/main/resources/db/migration/V0006000__create_ArchivedMember_tables.sql
@@ -11,7 +11,5 @@ CREATE TABLE tbl_member_history (
     guild_id bigint NOT NULL,
     timestamp bigint NOT NULL,
     nickname text,
-    PRIMARY KEY (user_id, guild_id),
-	FOREIGN KEY (user_id, guild_id) REFERENCES tbl_member_history(user_id, guild_id),
     FOREIGN KEY (guild_id) REFERENCES tbl_guild_config(guild_id)
 );
