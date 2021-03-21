0dba19a1bd3231b0868997c0f6f4e799306e22f2
Default <HunHage@gmail.com>
2021-03-21 14:36:29 -0500

    remove limits on file name and extension

--- src/main/resources/db/migration/V0002000__create_ArchivedFile_table.sql
+++ src/main/resources/db/migration/V0002000__create_ArchivedFile_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tbl_files (
     pk_md5 varchar(32) NOT NULL UNIQUE,
-    name varchar(256) NOT NULL,
-    extension varchar(16) NOT NULL,
+    name varchar NOT NULL,
+    extension varchar NOT NULL,
     urls varchar(2048)[],
     data_bytes bytea NOT NULL,
     PRIMARY KEY (pk_md5)
