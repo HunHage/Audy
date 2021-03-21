f786980bdb000572d91ae502d5db7e2a591ecfee
Default <HunHage@gmail.com>
2021-03-21 14:40:40 -0500

    remove limits on url length

--- src/main/resources/db/migration/V0002000__create_ArchivedFile_table.sql
+++ src/main/resources/db/migration/V0002000__create_ArchivedFile_table.sql
@@ -2,7 +2,7 @@ CREATE TABLE tbl_files (
     pk_md5 varchar(32) NOT NULL UNIQUE,
     name varchar NOT NULL,
     extension varchar NOT NULL,
-    urls varchar(2048)[],
+    urls varchar[],
     data_bytes bytea NOT NULL,
     PRIMARY KEY (pk_md5)
 );
